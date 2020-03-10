1990ac6a505f4d545fbf624085acd93e3354958d
Thomas West <westthomas@hotmail.co.uk>
2020-03-10 13:37:13 +0000

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,10 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source = "app.terraform.io/TWEST-training/vpc/aws"
 
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1"]
+  azs             = ["us-east-1a"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
